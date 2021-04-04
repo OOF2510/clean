70ffac553e354f02f1e238c6ae2b224572c165da
Nate T <64423517+OOF2510@users.noreply.github.com>
2021-04-04 18:48:29 -0400

    Create clean.sh

---
+++ clean.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+sudo df -h && sudo du -sh /var/cache/apt && journalctl --disk-usage && sudo apt -y autoremove && sudo apt -y autoclean && sudo apt -y clean && sudo journalctl --vacuum-time=3d && sudo du -sh /var/cache/apt && sudo df -h
